43bda8b27965dcfa8ced0431446fdda26837b6c5
FelisbertoJPG <joaopfschmidt@gmail.com>
2025-10-02 11:58:42 -0300

    feat: camada de Exceptions

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,13 +1,22 @@
-// lib/repositories/auth_repository.dart
-import '../services/dev_api.dart';
+// lib/repositories/auth_repository.dart (trecho relevante)
+import 'package:dio/dio.dart';
+import '../core/app_exception.dart';
+import '../services/dio_error_mapper.dart';
 import '../models/profile.dart';
+import '../services/dev_api.dart';
 
 class AuthRepository {
   final DevApi api;
   AuthRepository(this.api);
 
   Future<Profile> login(String cpf, String senha) async {
-    final map = await api.login(cpf: cpf, senha: senha);
-    return Profile.fromMap(map);
+    try {
+      final m = await api.login(cpf: cpf, senha: senha);
+      return Profile.fromMap(m);
+    } on DioException catch (e) {
+      throw mapDioError(e);
+    } catch (_) {
+      throw const UnexpectedException();
+    }
   }
 }
